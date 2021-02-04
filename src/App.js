e8eb9b1d6eb692bccb49e4590cdf1586ce27bec1
Ronald Amoo <butsormeamoo@gmail.com>
2021-02-04 13:56:08 +0000

    passed a prop to a component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class WelcomeComponent extends React.Component {
+  render() {
+    return <h1>{this.props.name}Is Awesome,React Works!!!!</h1>;
+  }
 }
-
+const App = () => {
+  let name = "Ronald";
+  return <WelcomeComponent name="Ronald" />;
+};
 export default App;
